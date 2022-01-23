250b28e7ffcb9435a262fc53c000f887e09c2fe6
indicium15 <67154087+indicium15@users.noreply.github.com>
2022-01-23 15:26:42 +0800

    Scene 2 Done

--- gamejam2022/Assets/Scripts/camera.cs
+++ gamejam2022/Assets/Scripts/camera.cs
@@ -13,7 +13,7 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moverCamX = 0.001F;
+        float moverCamX = 0.002F;
         Camera.main.transform.Translate(moverCamX, 0, 0);
         
     }
