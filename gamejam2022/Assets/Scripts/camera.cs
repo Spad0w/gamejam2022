984877750221eaf9c5f6d7b8a19c773bb0ed21bb
DanielxDante <danielonline170600@outlook.com>
2022-01-23 16:18:49 +0800

    fix stage 1

--- gamejam2022/Assets/Scripts/camera.cs
+++ gamejam2022/Assets/Scripts/camera.cs
@@ -13,11 +13,8 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         float moverCamX = 0.003F;
-=======
-        float moverCamX = 0.002F;
->>>>>>> 250b28e7ffcb9435a262fc53c000f887e09c2fe6
+
         Camera.main.transform.Translate(moverCamX, 0, 0);
         
     }
